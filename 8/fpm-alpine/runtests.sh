d5384aef8637c5e4d88b139951353c43714cfea8
Marcelo Vani <marcellovani@yahoo.co.uk>
2019-02-15 18:55:23 +0000

    List verbose files

--- 8/fpm-alpine/runtests.sh
+++ 8/fpm-alpine/runtests.sh
@@ -32,3 +32,4 @@ composer require drupal/${require_module}:${module_version}
 
 php core/scripts/drupal install ${drupal_profile}
 php core/scripts/run-tests.sh --php /usr/local/bin/php --color --concurrency "31" --sqlite sites/default/files/.ht.sqlite --verbose --directory "modules/contrib/${require_module}"
+ls sites/default/files/simpletest/verbose

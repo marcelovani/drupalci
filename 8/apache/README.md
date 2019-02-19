ea941e3f5b6397e0e3e98f692f6215c89bbe7657
Marcelo Vani <marcellovani@yahoo.co.uk>
2019-02-19 16:54:57 +0000

    Added apache image

---
+++ 8/apache/README.md
@@ -0,0 +1,76 @@
+# DrupalCi
+
+Runs Drupal webtests and php unit tests using Docker containers.
+This can be used with Git webhooks but its not ready yet.
+
+## Usage examples
+The parameters for the runtests.sh script are:
+* --profile   Drupal install profile [optinoal, default = 'minimal']
+* --project   Project or module name
+* --version   Project version or branch name
+* --vcs       Fork url [optional]
+
+Run tests from the a released version of Captcha Keypad module
+
+```bash
+docker run --name drupalci --rm marcellovani/drupalci:8-apache /bin/sh -c "php runtests.php --project captcha_keypad --version 1.x"
+```
+
+Run tests from the main dev branch of Captcha Keypad module
+
+```bash
+docker run --name drupalci --rm marcellovani/drupalci:8-apache /bin/sh -c "php runtests.php --project drupal/captcha_keypad --version 1.x-dev"
+```
+
+Run tests from the a forked branch of Captcha Keypad module
+
+```bash
+docker run --name drupalci --rm marcellovani/drupalci:8-apache /bin/sh -c "php runtests.php --profile standard --project captcha_keypad --version broken_test-dev --vcs https://github.com/marcelovani/captcha_keypad.git"
+```
+
+Keeping the results
+
+
+```bash
+docker run -v /tmp/drupalci/verbose:/var/www/html/sites/default/files/simpletest/verbose --name drupalci --rm marcellovani/drupalci:8-apache /bin/sh -c "php runtests.php --project captcha_keypad --branch 8.x-1.x-dev --vcs https://github.com/marcelovani/captcha_keypad.git"
+
+ls /tmp/drupalci/verbose
+```
+
+Viewing the results
+
+
+```bash
+ls /tmp/drupalci/verbose
+```
+
+### Using is on your Drupal module
+
+Copy the .circleci folder into your module, remame config.yml.example to config.yml and enable Circle CI for your project. When you make commits it will automatically trigger the build an you will be able to access the verbose results via Artifacts tab on Circle CI.
+
+
+### This is standard Docker stuff
+
+Building an image
+
+```
+docker build -t marcellovani/drupalci:8-apache .
+```
+
+Pushing a tag
+
+```
+docker tag drupal:8-apache drupalci:8-apache
+docker push marcellovani/drupalci:8-apache
+```
+
+## Todos
+[ ] Add support for Drupal 7
+
+[ ] Support patches
+
+[ ] PHP script needs to return code 1 when tests fail
+
+[x] Access to results of tests
+
+[x] Pass profile in the arguments or read from the tests
---
+++ 8/apache/runtests.php
@@ -0,0 +1,108 @@
+<?php
+
+/**
+ * @file
+ * This script runs Drupal tests from command line.
+ */
+
+// Set defaults and get overrides.
+list($args) = script_parse_args();
+//print_r ($args);
+
+// Validate project
+if (empty ($args['project'])) {
+  die("You need to specify the Drupal project i.e. --project drupal/captcha_keypad");
+}
+
+// Validate version
+if (empty ($args['version'])) {
+  die("You need to specify the project version or dev branch name i.e. --version 8.x-1.x-dev");
+}
+
+// Prepend vendor if needed
+if (strpos($args['project'], '/') !== FALSE) {
+	$args['require_project'] = $args['project'];
+}
+else {
+	$args['require_project'] = 'drupal/' . $args['project'];
+}
+
+// Prepare composer_commands
+$composer_commands = [];
+if (!empty ($args['vcs'])) {
+  $options = array(
+    'type' => 'vcs',
+    'url' => $args['vcs'],
+  );
+
+  if (strpos($args['vcs'], 'https') !== false) {
+    $options['no-api'] = true;
+  }
+
+  $composer_commands[] = 'composer config repositories.' . $args['project'] . ' \'' . json_encode($options, JSON_UNESCAPED_SLASHES) . '\'';
+}
+$composer_commands[] = 'composer require ' . $args['require_project'] . ':' . $args['version'];
+
+// Prepare all commands.
+$commands = $composer_commands;
+$commands[] = 'php core/scripts/drupal install ' . $args['profile'];
+$commands[] = 'php core/scripts/run-tests.sh --php /usr/local/bin/php --keep-results --color --concurrency "31" --sqlite sites/default/files/.ht.sqlite --verbose --directory "modules/contrib/' . $arg['project'] . '"';
+
+//print_r ($args);
+//print_r ($commands);
+
+// Run commands.
+foreach ($commands as $command) {
+  passthru($command, $err);
+  if ($err != 0) {
+    exit($err);
+  }
+}
+
+/**
+ * Parse execution argument and ensure that all are valid.
+ *
+ * @return array
+ *   The list of arguments.
+ */
+function script_parse_args() {
+  // Set default values.
+  $args = [
+    'script' => '',
+    'help' => FALSE,
+    'profile' => 'minimal',
+    'project' => '',
+	'version' => '',
+	'vcs' => '',
+  ];
+
+  // Override with set values.
+  $args['script'] = basename(array_shift($_SERVER['argv']));
+
+  while ($arg = array_shift($_SERVER['argv'])) {
+    if (preg_match('/--(\S+)/', $arg, $matches)) {
+      // Argument found.
+      if (array_key_exists($matches[1], $args)) {
+        // Argument found in list.
+        $previous_arg = $matches[1];
+        if (is_bool($args[$previous_arg])) {
+          $args[$matches[1]] = TRUE;
+        }
+        elseif (is_array($args[$previous_arg])) {
+          $value = array_shift($_SERVER['argv']);
+          $args[$matches[1]] = array_map('trim', explode(',', $value));
+        }
+        else {
+          $args[$matches[1]] = array_shift($_SERVER['argv']);
+        }
+      }
+      else {
+        // Argument not found in list.
+        echo ("Unknown argument '$arg'.");
+        exit(1);
+      }
+    }
+  }
+
+  return [$args];
+}

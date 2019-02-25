833960444ecba62b4e03367d42d7787dd0d6b2cc
Marcelo Vani <marcellovani@yahoo.co.uk>
2019-02-25 10:44:04 +0000

    Fixed tag issue that was breaking builds

--- build_deploy_all.sh
+++ build_deploy_all.sh
@@ -4,16 +4,13 @@
 sh build_all.sh
 
 cd ./7/apache \
-  && docker tag drupal:7-apache marcelovani/drupalci:7-apache \
   && docker push marcelovani/drupalci:7-apache \
   && cd ../../
 
 cd ./7/apache-interactive \
-  && docker tag drupal:7-apache marcelovani/drupalci:7-apache-interactive \
   && docker push marcelovani/drupalci:7-apache-interactive \
   && cd ../../
 
 cd ./8/apache \
-  && docker tag drupal:8-apache marcelovani/drupalci:8-apache \
   && docker push marcelovani/drupalci:8-apache \
   && cd ../../
